ba56135b83228cba8354e7fa8ce8692ce180a1f1
Sam Cheng (Tata Consultancy Services) <v-samnac@microsoft.com>
2019-05-28 22:49:55 -0700

    Complete music keyboard

--- noise.js
+++ noise.js
@@ -1,3 +1,18 @@
 $(document).ready( function() {
-  // your code here
+  $("button.note").on('click', function(event) {
+    const myAudio = $("#" + event.currentTarget.textContent + "Audio");
+    playAudio(myAudio);
+  });
 });
+
+document.addEventListener('keydown', function(event) {
+  let myAudio = $('#'+ event.key + 'Audio');
+  playAudio(myAudio);
+});
+
+function playAudio(audioComponent) {
+  if (audioComponent && audioComponent.length > 0) {
+    audioComponent[0].currentTime = 0;
+    audioComponent[0].play();
+  }
+}
